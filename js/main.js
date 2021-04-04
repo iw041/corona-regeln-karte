06a61b75ed92241c969c3b123323f19aedd423e8
Ingolf <ingolf@iw041.de>
2021-04-04 14:49:05 +0200

    added pan and zoom functionality for SVG map

--- js/main.js
+++ js/main.js
@@ -5,7 +5,39 @@ window.onload = () => {
       navigator.serviceWorker
                .register('./serviceworker.js');
     }
-  }
+
+    // ################ Pan and zoom for SVG map ################
+    window.panZoomInstance = svgPanZoom('#map-svg', {
+      zoomEnabled: true,
+      controlIconsEnabled: false,
+      fit: true,
+      center: true,
+      minZoom: 0.5
+    });
+
+    // Zoom out
+    //panZoomInstance.zoom(0.5);
+
+    function customPanBy(amount){ // {x: 1, y: 2}
+      var animationTime = 300 // ms
+        , animationStepTime = 15 // one frame per 30 ms
+        , animationSteps = animationTime / animationStepTime
+        , animationStep = 0
+        , intervalID = null
+        , stepX = amount.x / animationSteps
+        , stepY = amount.y / animationSteps
+
+      intervalID = setInterval(function(){
+        if (animationStep++ < animationSteps) {
+          panZoomInstance.panBy({x: stepX, y: stepY})
+        } else {
+          // Cancel interval
+          clearInterval(intervalID)
+        }
+      }, animationStepTime)
+    }
+
+}
 
 function displayName(e) {
   document.getElementById('district-name').firstChild.data = e.id;
