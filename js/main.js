64268772f79dbaab8a0fc723946b14cc84fa4851
Ingolf <ingolf@iw041.de>
2021-04-04 15:03:39 +0200

    removed useless code and added dynamic map size based on viewport

--- js/main.js
+++ js/main.js
@@ -8,35 +8,17 @@ window.onload = () => {
 
     // ################ Pan and zoom for SVG map ################
     window.panZoomInstance = svgPanZoom('#map-svg', {
+      panEnabled: true,
       zoomEnabled: true,
+      dblClickZoomEnabled: true,
+      mouseWheelZoomEnabled: true,
       controlIconsEnabled: false,
       fit: true,
       center: true,
-      minZoom: 0.5
+      zoomScaleSensitivity: 0.5,
+      minZoom: 1.0,
+      maxZoom: 10
     });
-
-    // Zoom out
-    //panZoomInstance.zoom(0.5);
-
-    function customPanBy(amount){ // {x: 1, y: 2}
-      var animationTime = 300 // ms
-        , animationStepTime = 15 // one frame per 30 ms
-        , animationSteps = animationTime / animationStepTime
-        , animationStep = 0
-        , intervalID = null
-        , stepX = amount.x / animationSteps
-        , stepY = amount.y / animationSteps
-
-      intervalID = setInterval(function(){
-        if (animationStep++ < animationSteps) {
-          panZoomInstance.panBy({x: stepX, y: stepY})
-        } else {
-          // Cancel interval
-          clearInterval(intervalID)
-        }
-      }, animationStepTime)
-    }
-
 }
 
 function displayName(e) {
